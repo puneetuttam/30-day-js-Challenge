38b1ae620f01620c2094203e0930a8de73e4b0a7
Puneet Uttam <puneet.uttam.99@gmail.com>
2024-07-15 09:41:45 +0530

    WeekDay

---
+++ Day3/Week.js
@@ -0,0 +1,36 @@
+function weekDay(num) {
+    switch (num) {
+        case 1:
+            console.log("Monday");
+            break;
+        case 2:
+            console.log("Tuesady");
+            break;
+        case 3:
+            console.log("Wednesday");
+            break;
+        case 4:
+            console.log("Thursday");
+            break;
+        case 5:
+            console.log("Friday");
+            break;
+        case 6:
+            console.log("Saturday");
+            break;
+        case 7:
+            console.log("Sunday");
+            break;
+        default:
+            console.log("Not a valid number")
+    }
+}
+
+weekDay(1);
+weekDay(2);
+weekDay(3);
+weekDay(4);
+weekDay(5);
+weekDay(6);
+weekDay(7);
+weekDay(8);

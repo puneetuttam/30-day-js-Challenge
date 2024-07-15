3197e3ab6c1be0bc1b15ac855398fd184113396f
Puneet Uttam <puneet.uttam.99@gmail.com>
2024-07-15 09:22:59 +0530

    Eligible to vote or not

---
+++ Day3/Age.js
@@ -0,0 +1,8 @@
+function isEligible(age) {
+    age >= 18
+        ? console.log("You are eligble to vote")
+        : console.log("You are not eligible to vote");
+}
+
+isEligible(12);
+isEligible(18);

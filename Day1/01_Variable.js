53d632bc2a727971be3de10c4328c9d6357e045b
Puneet Uttam <puneet.uttam.99@gmail.com>
2024-07-18 08:59:47 +0530

    Day1

---
+++ Day1/01_Variable.js
@@ -0,0 +1,38 @@
+// Activity 1: Variable Declaration
+// Task 1: 
+var x=100;
+console.log(x)
+// Task 2: 
+
+// Activity 2: Constant Declaration
+// Task 3: 
+const y=300
+
+//Activity 3: Data Types
+//Task 4:
+
+let n=1
+let b=true
+let obj={
+    dog: "bark",
+    cat: "meow"
+}
+let ar=[1,3,4,5,6,7]
+console.log(typeof n)
+console.log(typeof b)
+console.log(typeof obj)
+console.log(typeof ar)
+
+//Activity 4: Reassigning Variables
+//Task 5
+let i=0
+console.log(i)
+i=false
+console.log(i)
+
+//Activity 5: Understanding const
+
+const id=123;
+console.log(id)
+// id=234;
+// console.log(id) It will give type error as we can't reassign value of const variable
